7c88a6f3ce23435c96d52ae5123765900f9f70f2
Layria <105751836+Layria-b@users.noreply.github.com>
2023-04-16 15:11:46 +0100

    uses hWnd to get spotifys window title

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
+
 import vrchatapi
 from vrchatapi.api import authentication_api
 from vrchatapi.models.two_factor_email_code import TwoFactorEmailCode
 from vrchatapi.models.two_factor_auth_code import TwoFactorAuthCode
 from vrchatapi.exceptions import UnauthorizedException
+from pythonosc import udp_client
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import requests
 from http.cookiejar import LWPCookieJar
-import configparser
-from spotipy.oauth2 import SpotifyOAuth
 import threading
 from threading import Event
-import time 
-import requests
-import os
-from pythonosc import udp_client
+from time import sleep 
+from configparser import RawConfigParser
+import win32gui
 
-config = configparser.RawConfigParser()
+config = RawConfigParser()
 config.read("info/details.ini")
 udpClient = udp_client.SimpleUDPClient("127.0.0.1", 9000)
 
@@ -23,7 +23,6 @@ client_id = config["SPOTIFY"]["client_id"]
 client_secret = config["SPOTIFY"]["client_secret"]
 redirect_uri = config["SPOTIFY"]["redirect_uri"]
 scope = 'user-read-playback-state'
-os.environ["lastTrackName"] = ""
 VRCCookieFilename = "info/VRCCookies.txt"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
@@ -36,21 +35,17 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
 
 def save_cookies(filename: str):
     cookie_jar = LWPCookieJar(filename=filename)
-    
     for cookie in api_client.rest_client.cookie_jar:
         cookie_jar.set_cookie(cookie)
-        
     cookie_jar.save()
         
 def load_cookies(filename: str):
     cookie_jar = LWPCookieJar(filename=filename)
-
     try:
         cookie_jar.load()
     except FileNotFoundError:
         cookie_jar.save()
         return
-    
     for cookie in cookie_jar:
         api_client.rest_client.cookie_jar.set_cookie(cookie)
 
@@ -60,11 +55,11 @@ try:
         
         auth_api = authentication_api.AuthenticationApi(api_client)
         current_user = auth_api.get_current_user()
+
         print("Found Valid Cookies!")
         print("logged in as", current_user.display_name, "! :3")
         user_api = vrchatapi.UsersApi(api_client)
 except UnauthorizedException:
-
     print("No Cookie Found, Using username/password to login!")
     configuration = vrchatapi.Configuration(
     username = config["VRC"]["username"],
@@ -73,12 +68,14 @@ except UnauthorizedException:
 
     with vrchatapi.ApiClient(configuration) as api_client:
         auth_api = authentication_api.AuthenticationApi(api_client)
+
         try:
             current_user = auth_api.get_current_user()
         except ValueError:
             auth_api.verify2_fa_email_code(two_factor_email_code=TwoFactorEmailCode(input("Enter the code sent to your email: ")))
             current_user = auth_api.get_current_user()
             user_api = vrchatapi.UsersApi(api_client)
+
         except UnauthorizedException as e:
             if UnauthorizedException.status == 200:
                 auth_api.verify2_fa(two_factor_auth_code=TwoFactorAuthCode(input("Enter your 2fa code: ")))
@@ -87,79 +84,88 @@ except UnauthorizedException:
                 print("Exception when calling API: %s\n", e)
         except vrchatapi.ApiException as e:
             print("Exception when calling API: %s\n", e)
+
     print("logged in as", current_user.display_name, "! :3")
     save_cookies(filename=VRCCookieFilename)
 
+def lyrics(showLyrics, songUrl:str, lastStartTimeMs:float):
+    print("Lyrics thread started")
+    showLyrics.set()
+    lastStartTimeMs = lastStartTimeMs/1000
 
-def lyrics(paused, songUrl:str, lastStartTimeMs:float):
-    print("lyrics thread started")
     requUrl = f"https://spotify-lyric-api.herokuapp.com/?url={songUrl}&autoplay=true"
     requ = requests.get(requUrl)
     requJson = requ.json()
-    lastStartTimeMs = lastStartTimeMs/1000
-    paused.clear() #paused = False
+    
     try:
         for lyric in range(len(requJson["lines"])):
-            if paused.is_set():
+
+            if not showLyrics.is_set():
                 print("Stopping lyrics thread")
-                print("Because song ended or was paused.")
-                paused.clear() #paused = False
-                print(paused.is_set())
+                print("Because song ended or was paused")
+                showLyrics.set() #showLyrics = False
+                print(showLyrics.is_set())
                 break
-            StartTimeMs = float(requJson["lines"][lyric]["startTimeMs"])/1000
+
+            StartTimeMs = float(requJson["lines"][lyric]["startTimeMs"]+100)/1000
             if lastStartTimeMs>StartTimeMs:
                 continue
-            time.sleep(StartTimeMs-lastStartTimeMs)
+            sleep(StartTimeMs-lastStartTimeMs)
             lastStartTimeMs = StartTimeMs
 
-            print(requJson["lines"][lyric]["words"]) #just used for testing replace with below 
-            #udpClient.send_message("/chatbox/input", [requJson["lines"][lyric]["words"], True, False])
+            print(requJson["lines"][lyric]["words"])
+            udpClient.send_message("/chatbox/input", [requJson["lines"][lyric]["words"], True, False])
     except KeyError:
-        print("No lyrics for current song.") #just used for testing replace with below 
-        #udpClient.send_message("/chatbox/input", ["No lyrics for current song.", True, False])
+        print("No lyrics for current song.")
+        udpClient.send_message("/chatbox/input", ["No lyrics for current song...", True, False])
+ 
+hWnd = win32gui.FindWindowEx(None, None, None, 'Spotify Free')
+while (hWnd == 0):
+  hWnd = win32gui.FindWindowEx(None, None, None, 'Spotify Free')
+  input("Getting spotify's hWnd, Make sure spotify is open and not playing any songs, Press any key to continue...")
+print("Found spotify's hWnd.")
 
-def spotifyAndStatus(paused):
+def spotifyAndStatus(showLyrics):
+    lastSpotifyWindowTitle = ""
     while True:
-        spotify_song_resp = sp.current_user_playing_track()
-        try:
-            if spotify_song_resp["is_playing"] == True and spotify_song_resp['item']['name'] != os.environ["lastTrackName"]:
-                paused.set() #paused = True (even though its not paused idk it works i think)
-                track_name = spotify_song_resp['item']['name']
-                os.environ["lastTrackName"] = track_name
-                artists = [artist for artist in spotify_song_resp['item']['artists']]
-                artist_names = ', '.join([artist['name'] for artist in artists])
-                song = f'{track_name} by {artist_names}'
-                song = song.translate({ord(c): '`' for c in "\'\""})
-                
-                status_description = config['VRC']['statusFormat'].format(song = song)
-                if len(status_description) > 28:
-                    shorten_by_status = len(status_description[:-28])
-                    status_description = f"{status_description[:-shorten_by_status]}..."
-                
-                update_user_request = vrchatapi.UpdateUserRequest(
-                status_description=status_description
-                )
-                bio_request = user_api.update_user(current_user.id, update_user_request=update_user_request)
-                
-                songUrl = spotify_song_resp["item"]["external_urls"]["spotify"]
-                lastStartTimeMs = spotify_song_resp['progress_ms']
-                t2 = threading.Thread(target=lyrics, args=(paused, songUrl, lastStartTimeMs,))
-                t2.start()
-            
-            elif spotify_song_resp["is_playing"] == True and spotify_song_resp['item']['name'] == os.environ["lastTrackName"] and paused.is_set(): #paused is true?
-                print('playing lyrics from paused song')
-                paused.clear() #paused = False
-                songUrl = spotify_song_resp["item"]["external_urls"]["spotify"]
-                lastStartTimeMs = spotify_song_resp['progress_ms']
-                t2 = threading.Thread(target=lyrics, args=(paused, songUrl, lastStartTimeMs,))
-                t2.start()
-            elif spotify_song_resp["is_playing"] == False and spotify_song_resp['item']['name'] == os.environ["lastTrackName"]:
-                paused.set() #paused = True
-        except TypeError:
-            time.sleep(1)
-        time.sleep(1)
+        spotifyWindowTitle = win32gui.GetWindowText(hWnd)
+        
+        if lastSpotifyWindowTitle in {"Spotify Free", "Spotify Premium"}:
+            print("Song paused, If theres lyrics please wait for the 'Stopping lyrics thread' message.")
+            showLyrics.clear()
+        
+        if spotifyWindowTitle != lastSpotifyWindowTitle: 
+            lastSpotifyWindowTitle = spotifyWindowTitle
 
-paused = Event()
-t1 = threading.Thread(target=spotifyAndStatus, args=(paused,))
+            SpotifyRequ = sp.current_user_playing_track()
+            try: 
+                if SpotifyRequ["is_playing"] == True:
+
+                    track_name = SpotifyRequ['item']['name']
+                    artists = [artist for artist in SpotifyRequ['item']['artists']]
+                    artist_names = ', '.join([artist['name'] for artist in artists])
+                    song = f'{track_name} by {artist_names}'
+                    song = song.translate({ord(c): '`' for c in "\'\""})
+                    
+                    status_description = config['VRC']['statusFormat'].format(song = song)
+                    status_description = (status_description[:29] + '...') if len(status_description) > 29 else status_description
+                    
+                    update_user_request = vrchatapi.UpdateUserRequest(
+                    status_description=status_description
+                    )
+                    bio_request = user_api.update_user(current_user.id, update_user_request=update_user_request)
+                    
+                    songUrl = SpotifyRequ["item"]["external_urls"]["spotify"]
+                    lastStartTimeMs = SpotifyRequ['progress_ms']
+                    print("Starting lyrics thread")
+                    t2 = threading.Thread(target=lyrics, args=(showLyrics, songUrl, lastStartTimeMs,))
+                    t2.start()
+            except TypeError:
+                sleep(1)
+                continue
+        sleep(1)
+
+showLyrics = Event()
+t1 = threading.Thread(target=spotifyAndStatus, args=(showLyrics,))
 print("Waiting for song to be played!")
 t1.start()
